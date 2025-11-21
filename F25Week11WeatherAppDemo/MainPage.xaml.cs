46c9aa0e4cb06a911c5e6681c49661e9b7e0a023
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2025-11-21 11:05:30 -0500

    complete

--- F25Week11WeatherAppDemo/MainPage.xaml.cs
+++ F25Week11WeatherAppDemo/MainPage.xaml.cs
@@ -4,14 +4,13 @@ namespace F25Week11WeatherAppDemo
 {
     public partial class MainPage : ContentPage
     {
-        
-
         public MainPage()
         {
             InitializeComponent();
-        }
 
-        
+            if (DeviceInfo.Platform == DevicePlatform.Android)
+                stack.Background = Brush.MediumPurple;
+        }
 
         private async void OnGetWeatherBtnClicked(object sender, EventArgs e)
         {
